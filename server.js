385953c097329409d90c81e2d5e989a0dda8012d
DarkDanny25 <edanuc15@gmail.com>
2025-05-06 19:52:00 -0600

    activando sitio web

--- server.js
+++ server.js
@@ -11,8 +11,14 @@ const userRoutes = require('./routes/user');
 dotenv.config();
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Conexión a la base de datos MongoDB
 mongoose.connect(process.env.MONGODB_URI)
